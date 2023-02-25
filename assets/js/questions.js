171297692d59dce2c43454bf2f82491f58fc84f6
Bh00fie <Bh00fie@gmail.com>
2023-02-25 00:28:54 +0000

    added question.js file with 7 questions, more questions can be added without changing the code

---
+++ assets/js/questions.js
@@ -0,0 +1,37 @@
+var questionObj = [
+    {
+        question:"What is the output of the following code: console.log(7 % 3);?",
+        answers: ["4","2","1","3"],
+        correctAnswer: "2"
+    },
+    {
+        question:"Which of the following is the correct way to declare a variable in JavaScript?",
+        answers: ["variable = 5;","var 5 = variable;","var variable = 5","variable == 5;"],
+        correctAnswer: "var variable = 5"
+    },
+    {
+        question:"What is the purpose of the typeof operator in JavaScript?",
+        answers: ["to check if a value is defined or not","to convert a value to a string","to check the data type of a value","to compare two values"],
+        correctAnswer: "to check the data type of a value"
+    },
+    {
+        question:"What is the output of the following code: console.log('2' + 3);",
+        answers: ["23","5","6","undefined"],
+        correctAnswer: "23"
+    },
+    {
+        question:"Which of the following is NOT a valid data type in JavaScript?",
+        answers: ["boolean","number","integer","string"],
+        correctAnswer: "integer"
+    },
+    {
+        question:"Which of the following is the correct way to define a function in JavaScript?",
+        answers: ["function = myFunction() {}","function myFunction() {}","Avar myFunction() = function {}","myFunction = function() {}"],
+        correctAnswer: "function myFunction() {}"
+    },
+    {
+        question:"What is the difference between the == and === operators in JavaScript?",
+        answers: ["== compares values and data types, while === only compares values","=== compares values and data types, while == only compares values","== and === are the same and can be used interchangeably","== and === are not valid operators in JavaScript"],
+        correctAnswer: "== compares values and data types, while === only compares values"
+    },
+]

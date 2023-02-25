435bfe2ed74b790382c151cadb722531feff2ca4
Bh00fie <Bh00fie@gmail.com>
2023-02-25 00:30:49 +0000

    created function to store highscore, score will be ordered into a descending order

--- assets/js/score.js
+++ assets/js/score.js
@@ -1,3 +1,25 @@
 // Declaring variables using ID from HTML
 var highscoreList = document.getElementById("highscores");
 var clearBtn = document.getElementById("clear");
+
+// Function to retrive stored initials and scores
+function showHighscores() {
+    var previousScore = JSON.parse(localStorage.getItem("scores"));
+
+    // Sorting the scores into descending order
+    if (previousScore !== null) {
+        previousScore.sort(function(a,b) {
+            return b.score - a.score;
+        });
+
+        // Clearing existing high scores
+        highscoreList.innerHTML = "";
+
+        // Writing the list of scores onto the page
+        for (var i = 0; i < previousScore.length; i++){
+            var listScore = document.createElement("li");
+            highscoreList.appendChild(listScore);
+            listScore.textContent = previousScore[i].initials + " - " + previousScore[i].score;
+        }
+    }
+}

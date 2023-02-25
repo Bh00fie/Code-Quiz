9ca22c6a97b0966d1a95930c9ce047dccaeefeb0
Bh00fie <Bh00fie@gmail.com>
2023-02-25 00:14:14 +0000

    Created timer function will be used to control the timer of the quiz, this will be used to end the game if score is less than zero

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -16,3 +16,15 @@ var questionIndex = 0
 var timeLeft = 75;
 var winAudio = new Audio("./assets/sfx/correct.wav");
 var loseAudio = new Audio("./assets/sfx/incorrect.wav");
+
+// Timer function to control time and stop the game if the score is lower than 0 or if time has finished
+function timer() {
+    var timerInterval = setInterval(function() {
+        timeLeft--;
+        time.textContent = timeLeft;
+        if (timeLeft <= 0 || questionIndex === Object.keys(questionObj).length) {
+            clearInterval(timerInterval);
+            endGame();
+        }
+    }, 1000);
+}
